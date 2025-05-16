3fac580667830a43975cd10ca96a76daf2de2ff9
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-05-16 17:18:45 +0700

    init

---
+++ Arrays & Hashing/217. Contains Duplicate.cpp
@@ -0,0 +1,25 @@
+#include<unordered_set>
+#include<vector>
+#include<algorithm>
+#include<stdio.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> memo;
+        for (auto num : nums) {
+            if (!memo.insert(num).second) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,1,1,3,3,4,3,2,4,2};
+    bool result = Solution().containsDuplicate(nums);
+    printf("%s", result ? "true" : "false");
+}
