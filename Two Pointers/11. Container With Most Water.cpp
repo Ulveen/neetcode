0ff81e376e9779ee8235814bf8d7372c8dd36fd5
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-05-19 11:04:15 +0700

    optimize 11

--- Two Pointers/11. Container With Most Water.cpp
+++ Two Pointers/11. Container With Most Water.cpp
@@ -9,27 +9,16 @@ class Solution {
     int maxArea(vector<int>& height) {
         int l = 0, r = height.size() - 1, result = 0;
         while (l < r) {
-            int curr = min(height[l], height[r]) * (r - l);
-            result = max(curr, result);
-            if (height[l] > height[r]) {
-                r--;
-            }
-            else if (height[r] > height[l]) {
-                l++;
-            }
-            else {
-                r--;
-                l++;
-            }
+            result = max(min(height[l], height[r]) * (r - l), result);
+            if (height[l] > height[r]) r--;
+            else l++;
         }
         return result;
     }
 };
- 
+
 int main() {
     vector<int> height = {1,8,6,2,5,4,8,3,7};
     int result = Solution().maxArea(height);
-
     cout << result;
-
 }
