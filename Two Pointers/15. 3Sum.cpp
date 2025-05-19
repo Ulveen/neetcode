d0e52d1983f6dc5d6327fa3b179d00091623ddf0
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-05-19 10:47:37 +0700

    optimize 15

--- Two Pointers/15. 3Sum.cpp
+++ Two Pointers/15. 3Sum.cpp
@@ -1,43 +1,33 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<unordered_set>
 #include<algorithm>
 
 using namespace std;
 
 class Solution {
-    private:
-    string getHash(int a, int b, int c) {
-        return to_string(a) + to_string(b) + to_string(c);
-    }
-
     public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-
-        unordered_set<string> memo;
-
         int len = nums.size();
 
         for (int i = 0; i < len - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1, k = len - 1;
             while (j < k) {
                 int currSum = nums[i] + nums[j] + nums[k];
-                if (currSum == 0) {
-                    if (memo.insert(getHash(nums[i], nums[j], nums[k])).second) {
-                        result.push_back({nums[i], nums[j], nums[k]});
-                    }
-                }
-                if (currSum < 0) {
+                if (currSum < 0) j++;
+                else if(currSum > 0) k--;
+                else {
+                    while(j + 1 < k && nums[j] == nums[j + 1]) j++;
+                    while(k - 1 > j && nums[k] == nums[k - 1]) k--;
+                    result.push_back({nums[i], nums[j], nums[k]});
                     j++;
-                    continue;
+                    k--;
                 }
-                k--;
             }
         }
-
         return result;
     }
 };
