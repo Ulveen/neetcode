6f23fab4f83b6735a2a83ba96ba1c619a6cd0850
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-06-05 14:35:59 +0700

    simplify

--- Sliding Window/121. Best Time to Buy and Sell Stock.cpp
+++ Sliding Window/121. Best Time to Buy and Sell Stock.cpp
@@ -7,17 +7,16 @@ class Solution
 public:
     int maxProfit(std::vector<int> &prices)
     {
-        int start = 0, end = 0, result = 0;
+        int start = 0, result = 0;
         for (int i = 1; i < prices.size(); i++)
         {
             if (prices[i] < prices[start])
             {
-                start = end = i;
+                start = i;
             }
             else
             {
-                end = i;
-                result = std::max(prices[end] - prices[start], result);
+                result = std::max(prices[i] - prices[start], result);
             }
         }
         return result;
