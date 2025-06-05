d45e193f4ead8be2bbe7cefc5aa5835e1c5876a8
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-06-05 16:31:51 +0700

    optimize 424

--- Sliding Window/424. Longest Repeating Character Replacement.cpp
+++ Sliding Window/424. Longest Repeating Character Replacement.cpp
@@ -1,72 +1,30 @@
 #include <iostream>
-#include <unordered_map>
-#include <set>
-
-struct Entry
-{
-    char ch;
-    int count;
-
-    bool operator<(const Entry &other) const
-    {
-        if (count != other.count)
-            return count > other.count;
-        return ch < other.ch;
-    }
-};
-
-class FrequencyTracker
-{
-    std::unordered_map<char, int> freq;
-
-public:
-    std::set<Entry> sorted;
-    void increment(char ch)
-    {
-        if (freq.count(ch))
-        {
-            sorted.erase(Entry{ch, freq[ch]});
-        }
-        freq[ch]++;
-        sorted.insert(Entry{ch, freq[ch]});
-    }
-
-    void decrement(char ch)
-    {
-        sorted.erase(Entry{ch, freq[ch]});
-        freq[ch]--;
-        if (freq[ch] > 0)
-            sorted.insert(Entry{ch, freq[ch]});
-    }
-};
 
 class Solution
 {
 public:
     int characterReplacement(std::string s, int k)
     {
-        int result = 0, left = 0, len = s.length();
-        FrequencyTracker ft;
+        int n = s.size();
+        int count[26] = {0};
+        int left = 0, maxCount = 0, answer = 0;
 
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < n; i++)
         {
-            char c = s[i];
-            ft.increment(c);
-            int subLen = i - left + 1;
-            auto e = *ft.sorted.begin();
+            char c = s[i] - 'A';
+            count[c]++;
+            maxCount = std::max(maxCount, count[c]);
 
-            if (subLen - e.count <= k)
-                continue;
-
-            result = std::max(result, subLen - 1);
-            while (i - left + 1 - e.count > k)
+            while ((i - left + 1) - maxCount > k)
             {
-                ft.decrement(s[left++]);
-                e = *ft.sorted.begin();
+                count[s[left] - 'A']--;
+                left++;
             }
+
+            answer = std::max(answer, i - left + 1);
         }
 
-        return std::max(result, len - left);
+        return answer;
     }
 };
 
