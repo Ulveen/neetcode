cee1309ca96333db236dcb5c06b59ad71655ef0d
Ulveen <131880915+Ulveen@users.noreply.github.com>
2025-06-05 14:22:46 +0700

    4

--- Binary Search/4. Median of Two Sorted Arrays.cpp
+++ Binary Search/4. Median of Two Sorted Arrays.cpp
@@ -8,56 +8,60 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        int m = nums1.size(), n = nums2.size();
-        double median = 0;
+        int m = nums1.size(), n = nums2.size(), length = m + n;
 
-        vector<int> combined;
-
-        int left = 0, right = 0;
-
-        while (left < m && right < n)
+        if (m > n)
         {
-            if (nums1[left] < nums2[right])
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int left = 0, right = m;
+
+        while (left <= right)
+        {
+            int i = (left + right) / 2;
+            int j = (length + 1) / 2 - i;
+
+            int maxLeft1 = i == 0 ? INT_MIN : nums1[i - 1];
+            int minRight1 = i == m ? INT_MAX : nums1[i];
+
+            int maxLeft2 = j == 0 ? INT_MIN : nums2[j - 1];
+            int minRight2 = j == n ? INT_MAX : nums2[j];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1)
             {
-                combined.push_back(nums1[left++]);
+                if (length % 2 == 1)
+                {
+                    return max(maxLeft1, maxLeft2);
+                }
+                return ((double)max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2;
+            }
+            else if (maxLeft1 > minRight2)
+            {
+                right = i - 1;
             }
             else
             {
-                combined.push_back(nums2[right++]);
+                left = i + 1;
             }
         }
 
-        while (left < m)
-        {
-            combined.push_back(nums1[left++]);
-        }
-
-        while (right < n)
-        {
-            combined.push_back(nums2[right++]);
-        }
-
-        int mid = m + n;
-        bool isOdd = mid % 2 == 1;
-        mid /= 2;
-
-        if (isOdd)
-        {
-            median = combined[mid];
-        }
-        else
-        {
-            median = ((double)combined[mid] + combined[mid - 1]) / 2;
-        }
-
-        return median;
+        return 0;
     }
 };
 
 int main()
 {
-    vector<int> nums1 = {1, 2};
-    vector<int> nums2 = {3, 4};
+    vector<int> nums1 = {1, 2, 3, 4, 5};
+    vector<int> nums2 = {1, 2, 3, 4, 5, 6, 7, 8};
     double result = Solution().findMedianSortedArrays(nums1, nums2);
     cout << result;
 }
+
+//               i
+// A = [1, 2, 3, 4, 5]
+//            j
+// B = [1, 2, 3, 4, 5, 6, 7, 8]
+// total = 13
+// mid = 6
+// median = 4
