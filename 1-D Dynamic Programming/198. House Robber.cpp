9229a9e9ebd9279cc398e81a6aec72a3061b196d
ulveen <setionoalvin9@gmail.com>
2025-12-15 14:08:06 +0700

    198

---
+++ 1-D Dynamic Programming/198. House Robber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int rob(vector<int> &nums) {
+    int n = nums.size();
+    if (n == 1)
+      return nums[0];
+    else if (n == 2)
+      return max(nums[0], nums[1]);
+
+    int dp[n];
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+
+    for (int i = 2; i < n; i++) {
+      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+
+    for (int num : nums) {
+      printf("%d ", num);
+    }
+    puts("");
+
+    return max(dp[n - 1], dp[n - 2]);
+  }
+};
+
+int main() {
+  vector<int> nums = {2, 7, 9, 3, 1};
+  int result = Solution().rob(nums);
+
+  printf("%d\n", result);
+}
