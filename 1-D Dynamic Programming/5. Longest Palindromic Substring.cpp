194af71e53c7252a4b2a7478bc10a42ebc3d4428
ulveen <setionoalvin9@gmail.com>
2025-12-16 17:23:07 +0700

    optimize 5

--- 1-D Dynamic Programming/5. Longest Palindromic Substring.cpp
+++ 1-D Dynamic Programming/5. Longest Palindromic Substring.cpp
@@ -4,31 +4,35 @@ using namespace std;
 
 class Solution {
 private:
-  bool isPalindrome(string &s, int l, int r) {
-    while (l < r) {
-      if (s[l++] != s[r--])
-        return false;
+  int traverse(string &s, int start, int end) {
+    while (start >= 0 && end < s.length() && s[start] == s[end]) {
+      start--;
+      end++;
     }
-    return true;
+    return end - start - 1;
   }
 
 public:
   string longestPalindrome(string s) {
-    int windowSize = s.length();
+    int start = 0;
+    int len = 1;
 
-    for (int i = windowSize; i > 0; i--) {
-      for (int j = 0; j <= s.length() - i; j++) {
-        if (isPalindrome(s, j, j + i - 1))
-          return s.substr(j, i);
+    for (int i = 0; i < s.length(); i++) {
+      int odd = traverse(s, i - 1, i + 1);
+      int even = traverse(s, i, i + 1);
+
+      int candidateLen = max(odd, even);
+      if (candidateLen > len) {
+        len = candidateLen;
+        start = i - (len - 1) / 2;
       }
     }
-
-    return "";
+    return s.substr(start, len);
   }
 };
 
 int main() {
-  string s = "babad";
+  string s = "bb";
   string result = Solution().longestPalindrome(s);
   cout << result << endl;
 }
