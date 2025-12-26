86f42886b2809745cb880511a165593bbfed02d3
ulveen <setionoalvin9@gmail.com>
2025-12-26 23:49:29 +0700

    Optimize 57

--- Intervals/57. Insert Interval.cpp
+++ Intervals/57. Insert Interval.cpp
@@ -7,73 +7,23 @@ class Solution {
 public:
   vector<vector<int>> insert(vector<vector<int>> &intervals,
                              vector<int> &newInterval) {
-    int n = intervals.size();
+
     vector<vector<int>> result;
+    int i = 0, n = intervals.size();
 
-    if (intervals.empty()) {
-      result.push_back(newInterval);
-      return result;
+    while (i < n && intervals[i][1] < newInterval[0]) {
+      result.push_back(intervals[i++]);
     }
 
-    int state = 0;
-    int idx = 0;
-
-    for (int i = 0; i < n; i++) {
-      vector<int> interval = intervals[i];
-
-      // continue search
-      if (interval[1] < newInterval[0]) {
-        result.push_back(interval);
-        continue;
-      }
-
-      idx = i;
-
-      // insert newInterval
-      if (interval[0] > newInterval[1]) {
-        state = 1;
-        result.push_back(newInterval);
-      }
-
-      // ignore newInterval
-      else if (interval[0] <= newInterval[0] && interval[1] >= newInterval[1]) {
-        state = 2;
-      }
-
-      // merge intervals
-      else {
-        state = 3;
-      }
-      break;
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+      newInterval[0] = min(newInterval[0], intervals[i][0]);
+      newInterval[1] = max(newInterval[1], intervals[i][1]);
+      i++;
     }
+    result.push_back(newInterval);
 
-    if (state == 0) {
-      result.push_back(newInterval);
-      return result;
-    }
-
-    if (state <= 2) {
-      while (idx < n) {
-        result.push_back(intervals[idx++]);
-      }
-      return result;
-    }
-
-    int startIdx = idx, endIdx = idx;
-    while (endIdx < n && intervals[endIdx][1] < newInterval[1]) {
-      endIdx++;
-    }
-
-    if (endIdx == n || intervals[endIdx][0] > newInterval[1]) {
-      endIdx--;
-    }
-
-    int minVal = min(intervals[startIdx][0], newInterval[0]);
-    int maxVal = max(intervals[endIdx][1], newInterval[1]);
-
-    result.push_back({minVal, maxVal});
-    while (++endIdx < n) {
-      result.push_back(intervals[endIdx]);
+    while (i < n) {
+      result.push_back(intervals[i++]);
     }
 
     return result;
